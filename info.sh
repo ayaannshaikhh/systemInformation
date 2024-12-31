40ad2ab04c6e18945c88713e585151060e3d197e
Ayaan Shaikh <ayaannshaikhh8@gmail.com>
2024-12-31 10:47:44 -0500

    Restored info.sh from master branch commit

---
+++ info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Current Date and Time: $(date '+%Y-%m-%d %H:%M:%S')"
+echo ""
+echo "Currently signed in as: $USER"
+echo "Current home directory: $HOME"
+echo "Current path to directory: $PWD"
+echo "Current hostname: $HOSTNAME"
+echo ""
+echo "Current bash version: $BASH_VERSION"
+echo "Current path to bash: $BASH"
+echo ""
+echo "Shell Uptime: $SECONDS secs"
+
